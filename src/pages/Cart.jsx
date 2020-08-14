33f70bbc6c588ebef9f72e5f3b19f3bfdbefb3f4
Murad <murad-arslangiraev-1994@mail.ru>
2020-08-14 19:46:58 +0300

    Сделан функционал увеличение количество в корзине и уменьшение, также мы в консоле получаем полный заказ при нажатий оплатить

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { CartItem } from '../components'
+import { CartItem, Button } from '../components'
 import {useSelector, useDispatch} from 'react-redux';
+import {Link} from 'react-router-dom'
+import cartEmptyImage from '../assets/img/empty-cart.png'
 
-import {clearCart} from '../redux/actions/cart'
+import {clearCart, removeCartItem, plusCartItem, minusCartItem} from '../redux/actions/cart'
 
 function Cart() {
     const dispatch = useDispatch();
@@ -18,9 +20,29 @@ function Cart() {
         }
     }
 
+
+    const onRemoveItem = (id) => {
+        console.log(id)
+        if (window.confirm("Вы действительно хотите удалить ?")) {
+            dispatch(removeCartItem(id))
+        }
+    }
+
+    const onPLusItem = (id) => {
+        dispatch(plusCartItem(id))
+    }
+
+    const onMinusItem = (id) => {
+        dispatch(minusCartItem(id))
+    }
+
+    const onMakeOrder = () => {
+        console.log("Ваш заказ", items)
+    }
     return (
         <div className="container container--cart">
-            <div className="cart">
+        {
+            totalCount ? <div className="cart">
                 <div className="cart__top">
                     <h2 className="content__title">
                         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -42,7 +64,18 @@ function Cart() {
                 </div>
                 <div className="content__items">
                     {
-                        addedPizzas.map(obj => <CartItem name={obj.name} type={obj.type} size={obj.size} totalPrice={items[obj.id].totalPrice} totalCount={items[obj.id].items.length}/>)
+                        addedPizzas.map(obj => <CartItem 
+                                                    key={obj.id}
+                                                    id={obj.id} 
+                                                    name={obj.name} 
+                                                    type={obj.type} 
+                                                    size={obj.size} 
+                                                    totalPrice={items[obj.id].totalPrice} 
+                                                    totalCount={items[obj.id].items.length}
+                                                    onRemove={onRemoveItem}
+                                                    onMinus={onMinusItem}
+                                                    onPlus={onPLusItem}
+                                                    />)
                     }
                 </div>
                 <div className="cart__bottom">
@@ -51,18 +84,29 @@ function Cart() {
                         <span> Сумма заказа: <b>{totalPrice} ₽</b> </span>
                     </div>
                     <div className="cart__bottom-buttons">
-                        <a href="/" className="button button--outline button--add go-back-btn">
-                        <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M7 13L1 6.93015L6.86175 1" stroke="#D3D3D3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                        </svg>
-                        <span>Вернуться назад</span>
-                        </a>
-                        <div className="button pay-btn">
-                        <span>Оплатить сейчас</span>
-                        </div>
+                        <Link to="/" className="button button--outline button--add go-back-btn">
+                            <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7 13L1 6.93015L6.86175 1" stroke="#D3D3D3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            </svg>
+                            <span>Вернуться назад</span>
+                        </Link>
+                        <Button onClick={onMakeOrder} className="button pay-btn">
+                            <span>Оплатить сейчас</span>
+                        </Button>
                     </div>
                 </div>
-            </div>
+            </div> : <div className="cart cart--empty">
+                        <h2>Корзина пустая <i>😕</i></h2>
+                        <p>
+                            Вероятней всего, вы не заказывали ещё пиццу.<br />
+                            Для того, чтобы заказать пиццу, перейди на главную страницу.
+                        </p>
+                        <img src={cartEmptyImage} alt="Empty cart" />
+                        <Link to="/" className="button button--black">
+                            <span>Вернуться назад</span>
+                        </Link>
+                    </div>
+        }
         </div>
     )
 }
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -6,6 +6,21 @@ const initialState = {
 
 const getTotalPrice = arr => arr.reduce((sum, obj) => obj.price + sum, 0);
 
+const _get = (obj, path) => {
+    const [firstKey, ...keys] = path.split('.');
+    return keys.reduce((val, key) => {
+        return val[key]
+    }, obj[firstKey])
+}
+
+const getTotalSum = (obj, path) => {
+    return Object.values(obj).reduce((sum, obj) => {
+        const value = _get(obj, path);
+        return sum + value;
+    }, 0)
+}
+
+
 const cart = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_PIZZA_CART':
@@ -19,15 +34,28 @@ const cart = (state = initialState, action) => {
                 }
             }
 
-            const items = Object.values(newItems).map((obj) => obj.items)
-            const allPizzas = [].concat.apply([], items)
-            const totalPrice = getTotalPrice(allPizzas)
+            const totalCount = getTotalSum(newItems, 'items.length')
+            const totalPrice = getTotalSum(newItems, 'totalPrice')
 
             return {
                 ...state,
                 items: newItems,
-                totalCount: allPizzas.length,
+                totalCount,
                 totalPrice
+            }
+        }
+        case 'REMOVE_CART_ITEM': {
+                const newItems = {
+                ...state.items
+            }
+            const currentTotalCount = newItems[action.payload].items.length;
+            const currentTotalPrice = newItems[action.payload].totalPrice;
+            delete newItems[action.payload]
+            return {
+                ...state,
+                items: newItems,
+                totalPrice: state.totalPrice - currentTotalPrice,
+                totalCount: state.totalCount - currentTotalCount
             }
         }
         case 'CLEAR_CART':
@@ -36,6 +64,56 @@ const cart = (state = initialState, action) => {
                 totalPrice: 0,
                 items: {}
             }
+
+        case 'PLUS_CART_ITEM':
+            {
+                const newObjItems = [
+                    ...state.items[action.payload].items,
+                    state.items[action.payload].items[0]
+                ];
+                const newItems = {
+                    ...state.items,
+                    [action.payload]: {
+                        items: newObjItems,
+                        totalPrice: getTotalPrice(newObjItems)
+                    },
+                }
+                const totalCount = getTotalSum(newItems, 'items.length')
+                const totalPrice = getTotalSum(newItems, 'totalPrice')
+
+
+                return {
+                    ...state,
+                    items: newItems,
+                    totalCount,
+                    totalPrice
+                }
+            }
+
+        case 'MINUS_CART_ITEM':
+            {
+                const oldItems = state.items[action.payload].items;
+                const newObjItems = oldItems.length > 1 ? state.items[action.payload].items.slice(1) : oldItems;
+                const newItems = {
+                    ...state.items,
+                    [action.payload]: {
+                        items: newObjItems,
+                        totalPrice: getTotalPrice(newObjItems)
+                    },
+                }
+                const totalCount = getTotalSum(newItems, 'items.length')
+                const totalPrice = getTotalSum(newItems, 'totalPrice')
+
+                return {
+                    ...state,
+                    items: newItems,
+                    totalPrice,
+                    totalCount
+                }
+            }
+
+        
+            
         default:
             return state;
     }
